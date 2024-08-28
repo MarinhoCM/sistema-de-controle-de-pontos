9e2f56609d2f77b71c2f6471b01c6fd69e702108
MarinhoCM <marinhoc384@gmail.com>
2024-08-28 15:15:42 -0300

    :sparkles: feat: acrescentando retorno de função a abstração

--- app/backend/arriegua/src/application/repositories/players.repository.ts
+++ app/backend/arriegua/src/application/repositories/players.repository.ts
@@ -2,7 +2,7 @@ import { PlayerEntity } from "../entities/player.entitie";
 
 export abstract class PlayerRepository {
     abstract create (player: PlayerEntity): Promise<PlayerEntity>;
-    abstract findById(id: string): Promise<PlayerEntity>;
+    abstract findById(id: string): Promise<PlayerEntity | null>;
     abstract findAll(): Promise<PlayerEntity[] | null>;
     abstract save(player: PlayerEntity): Promise<void>; 
 }
